455ea85e25c2e308fcc7cd3d82d88db03f74f18c
Gamila Toubar <36114894+gamilaaly@users.noreply.github.com>
2019-11-02 22:13:19 +0200

    Bluetooth for python and Arduino

--- Bluetooth.py
+++ Bluetooth.py
@@ -1,6 +1,6 @@
 import serial
 import time
-s = serial.Serial('COM10', 9600,timeout = 1) # choose the outgoing one 
+s = serial.Serial('COM10', 9600,timeout = 1) # choose the outgoing one
 print("connected!")
 time.sleep(2)
 s.write(b"f")
